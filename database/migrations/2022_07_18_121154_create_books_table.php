931f736407842e84be4b3c6d45ab711586cac16f
Midrogan <ikushpetyuk@mail.ru>
2022-07-18 20:18:21 +0600

    added books, series, authors, tags, genres migrations

---
+++ database/migrations/2022_07_18_121154_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('series_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->float('score', 2, 1);
+            $table->smallInteger('year');
+            $table->tinyInteger('part');
+            $table->text('annotation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
